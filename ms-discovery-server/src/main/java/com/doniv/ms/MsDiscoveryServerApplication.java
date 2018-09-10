20d446ea7bc470f9249083c3ecfcc72dd1855980
vinodkumarv <vinodkumar.vasudevan@gmail.com>
2018-09-10 19:27:25 +0800

    Initial Setup

---
+++ ms-discovery-server/src/main/java/com/doniv/ms/MsDiscoveryServerApplication.java
@@ -0,0 +1,12 @@
+package com.doniv.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDiscoveryServerApplication.class, args);
+	}
+}
---
+++ ms-sidecar/src/main/java/com/doniv/ms/MsSidecarApplication.java
@@ -0,0 +1,12 @@
+package com.doniv.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSidecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSidecarApplication.class, args);
+	}
+}
